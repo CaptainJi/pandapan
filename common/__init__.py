fc6a7ccc10043a06a078ce255c88c6f7d61bdfa4
CaptainJi <jiqing19861123@163.com>
2023-11-04 21:25:38 +0800

    init

---
+++ common/__init__.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+"""
+-*- coding: UTF-8 -*-
+Project   : pandapan
+Author    : Captain
+Email     : qing.ji@extremevision.com.cn
+Date      : 2023/11/4 7:54
+FileName  : __init__.py.py
+Software  : PyCharm
+Desc      : $END$
+"""
---
+++ configs/__init__.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+"""
+-*- coding: UTF-8 -*-
+Project   : pandapan
+Author    : Captain
+Email     : qing.ji@extremevision.com.cn
+Date      : 2023/11/4 8:31
+FileName  : __init__.py.py
+Software  : PyCharm
+Desc      : $END$
+"""
+from .basic_config import *
+from .model_config import *
+from .kb_config import *
+from .server_config import *
+from .prompt_config import *
